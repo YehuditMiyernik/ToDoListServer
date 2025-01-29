fd61c8d725f5872b98f4b595dbe7ae77db36b440
y0533121238@gmail.com <y0533121238@gmail.com>
2025-01-29 12:59:27 +0200

    add docker file

--- Models/TodoContext.cs
+++ Models/TodoContext.cs
@@ -21,7 +21,7 @@ public partial class TodoContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=todo;user=root;password=1234", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
+        => optionsBuilder.UseMySql("server=bqt86x6bdvifnvqptgxf-mysql.services.clever-cloud.com;user=u7hgokiun9w59snd;password=McqePFzcec3qvFgoyZOR;database=bqt86x6bdvifnvqptgxf", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf2696a0fac37696bc24c2556d53d3a83536bf71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06dc4942e6b64b10ec99d17f6db773946dba29ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
